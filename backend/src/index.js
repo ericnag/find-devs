9f70d82c93f2ebc0d14ba8b8c190c275b487d475
Eric Nagamuta <enagamuta@gmail.com>
2020-01-20 18:09:03 -0400

    Web Update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require('./routes');
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0-qfqsz.mongodb.net/find-devs?retryWrites=true&w=majority', {
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0-qfqsz.mongodb.net/find-devs?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+  }
+);
 
+app.use(
+  cors()
+);
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,116 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
 
+import DevForm from './components/DevForm/index';
+import DevItem from './components/DevItem/index';
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/26968294?s=460&v=4"
-                alt="Eric Nagamuta"
-              />
-              <div className="user-info">
-                <strong>Eric Nagamuta</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvedor Full Stack apaixonado em aprender novas tecnologias
-              e encarar novos desafios.
-            </p>
-            <a href="https://github.com/ericnag">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/26968294?s=460&v=4"
-                alt="Eric Nagamuta"
-              />
-              <div className="user-info">
-                <strong>Eric Nagamuta</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvedor Full Stack apaixonado em aprender novas tecnologias
-              e encarar novos desafios.
-            </p>
-            <a href="https://github.com/ericnag">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/26968294?s=460&v=4"
-                alt="Eric Nagamuta"
-              />
-              <div className="user-info">
-                <strong>Eric Nagamuta</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvedor Full Stack apaixonado em aprender novas tecnologias
-              e encarar novos desafios.
-            </p>
-            <a href="https://github.com/ericnag">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/26968294?s=460&v=4"
-                alt="Eric Nagamuta"
-              />
-              <div className="user-info">
-                <strong>Eric Nagamuta</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvedor Full Stack apaixonado em aprender novas tecnologias
-              e encarar novos desafios.
-            </p>
-            <a href="https://github.com/ericnag">Acessar perfil no Github</a>
-          </li>
-
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
