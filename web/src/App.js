97d9f0aec02b03836e9669a09b711554a9f8bad0
Eric Nagamuta <enagamuta@gmail.com>
2020-01-20 12:08:00 -0400

    Front-End Web Created

---
+++ web/src/App.js
@@ -0,0 +1,120 @@
+import React from "react";
+
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/26968294?s=460&v=4"
+                alt="Eric Nagamuta"
+              />
+              <div className="user-info">
+                <strong>Eric Nagamuta</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>
+              Desenvolvedor Full Stack apaixonado em aprender novas tecnologias
+              e encarar novos desafios.
+            </p>
+            <a href="https://github.com/ericnag">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/26968294?s=460&v=4"
+                alt="Eric Nagamuta"
+              />
+              <div className="user-info">
+                <strong>Eric Nagamuta</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>
+              Desenvolvedor Full Stack apaixonado em aprender novas tecnologias
+              e encarar novos desafios.
+            </p>
+            <a href="https://github.com/ericnag">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/26968294?s=460&v=4"
+                alt="Eric Nagamuta"
+              />
+              <div className="user-info">
+                <strong>Eric Nagamuta</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>
+              Desenvolvedor Full Stack apaixonado em aprender novas tecnologias
+              e encarar novos desafios.
+            </p>
+            <a href="https://github.com/ericnag">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/26968294?s=460&v=4"
+                alt="Eric Nagamuta"
+              />
+              <div className="user-info">
+                <strong>Eric Nagamuta</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>
+              Desenvolvedor Full Stack apaixonado em aprender novas tecnologias
+              e encarar novos desafios.
+            </p>
+            <a href="https://github.com/ericnag">Acessar perfil no Github</a>
+          </li>
+
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
