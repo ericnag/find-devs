b45aba14d43288a0dabadb65ca6cb4aee8a8ddab
Eric Nagamuta <enagamuta@gmail.com>
2020-01-20 22:34:06 -0400

    Mobile Created

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Image } from "react-native";
+import MapView, { Marker, Callout } from "react-native-maps";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
+
+function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        });
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker
+        coordinate={{ latitude: -22.2240004, longitude: -54.7945332 }}
+      >
+          <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/26968294?s=460&v=4'}}/>
+      </Marker>
+    </MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+
+  avatar: {
+      width: 54,
+      height: 54,
+      borderRadius: 4,
+      borderWidth: 4,
+      borderColor: '#FFF'
+  }
+});
+
+export default Main;
